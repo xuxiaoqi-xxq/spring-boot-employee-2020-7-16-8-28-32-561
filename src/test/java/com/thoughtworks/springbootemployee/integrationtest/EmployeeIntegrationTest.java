fa095e9e6916909c45a7a2d8f7b06c4ab3406f50
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-30 16:01:25 +0800

    [EmployeeIntegration][eva & christopher] feature: add test should_specific_employees_when_hit_employees_endpoint_given_employee_id

--- src/test/java/com/thoughtworks/springbootemployee/integrationtest/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationtest/EmployeeIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -34,6 +35,21 @@ public class EmployeeIntegrationTest {
     }
 
     @Test
+    void should_specific_employees_when_hit_employees_endpoint_given_employee_id() throws Exception {
+        //given
+        Employee savedEmployee = employeeRepository.save(new Employee(1, 18, "female", "xxx", 10000));
+
+        mockMvc.perform(get("/employees/" + savedEmployee.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.name").value("xxx"))
+                .andExpect(jsonPath("$.age").value(18))
+                .andExpect(jsonPath("$.salary").value(10000))
+                .andExpect(jsonPath("$.gender").value("female"));
+    }
+
+
+    @Test
     void should_return_all_employees_when_hit_employees_endpoint_given_nothing() throws Exception {
         //given
         List<Employee> employees = Arrays.asList(new Employee(1, 18, "male", "xxx", 1000));
@@ -49,4 +65,39 @@ public class EmployeeIntegrationTest {
                 .andExpect(jsonPath("$[0].gender").value("male"));
 
     }
+
+    @Test
+    void should_page_employees_when_hit_employees_endpoint_given_page_and_pageSize() throws Exception {
+        //given
+        List<Employee> employees = Collections.singletonList(new Employee(1, 18, "male", "xxx", 1000));
+        employeeRepository.saveAll(employees);
+
+        mockMvc.perform(get("/employees?page=1&pageSize=1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(1)))
+                .andExpect(jsonPath("$.content[0].id").isNumber())
+                .andExpect(jsonPath("$.content[0].name").value("xxx"))
+                .andExpect(jsonPath("$.content[0].age").value(18))
+                .andExpect(jsonPath("$.content[0].salary").value(1000))
+                .andExpect(jsonPath("$.content[0].gender").value("male"));
+    }
+
+    @Test
+    void should_gender_employees_when_hit_employees_endpoint_given_gender() throws Exception {
+        //given
+        List<Employee> employees = Arrays.asList(new Employee(1, 18, "male", "xxx", 1000),
+                new Employee(2, 18, "female", "xxx", 10000));
+        employeeRepository.saveAll(employees);
+
+        mockMvc.perform(get("/employees?gender=female"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].name").value("xxx"))
+                .andExpect(jsonPath("$[0].age").value(18))
+                .andExpect(jsonPath("$[0].salary").value(10000))
+                .andExpect(jsonPath("$[0].gender").value("female"));
+    }
+
+
 }
